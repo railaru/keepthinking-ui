763f7a33052dd49519012161592a3ca5922dbb8c
Rutenis Raila <rutis98@gmail.com>
2019-03-13 22:31:13 +0000

    Updates

---
+++ precache-manifest.5c90669bf7b8cbd1fce4deb71545a59f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "57ea58a67328cbea65f3f4f69f64d6bd",
+    "url": "/keepthinking-ui/static/media/logo.57ea58a6.png"
+  },
+  {
+    "revision": "ce720e094f608e2405c4",
+    "url": "/keepthinking-ui/static/js/runtime~main.ce720e09.js"
+  },
+  {
+    "revision": "8ca3f2c9322aaffd72eb",
+    "url": "/keepthinking-ui/static/js/main.8ca3f2c9.chunk.js"
+  },
+  {
+    "revision": "21a84e97181494580a3b",
+    "url": "/keepthinking-ui/static/js/2.21a84e97.chunk.js"
+  },
+  {
+    "revision": "8ca3f2c9322aaffd72eb",
+    "url": "/keepthinking-ui/static/css/main.eed27d16.chunk.css"
+  },
+  {
+    "revision": "21a84e97181494580a3b",
+    "url": "/keepthinking-ui/static/css/2.4a1065d1.chunk.css"
+  },
+  {
+    "revision": "af0dcef61053bbe2141a975fb9a90354",
+    "url": "/keepthinking-ui/index.html"
+  }
+];
